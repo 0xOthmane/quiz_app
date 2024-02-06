9c0f5ad5b5758a1b8052c203012902f1c0a15e3d
0xOthmane <0xothodor@proton.me>
2024-02-06 11:24:33 +0100

    feat: add questions listing in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,40 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                "titre" => "Title",
+                "contenu" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum asperiores quod quis nisi, ab beatae ipsum sint obcaecati ipsam, modi officia excepturi voluptas, corporis at tempore fugit exercitationem repudiandae ipsa?",
+                "vote" => 4,
+                "auteur" => [
+                    "nom" => "Joe Doe",
+                    "img_url" => "https://i.pravatar.cc/300"
+                ],
+                "nbr_reponses" => 4
+            ],
+            [
+                "titre" => "Title",
+                "contenu" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum asperiores quod quis nisi, ab beatae ipsum sint obcaecati ipsam, modi officia excepturi voluptas, corporis at tempore fugit exercitationem repudiandae ipsa?",
+                "vote" => 0,
+                "auteur" => [
+                    "nom" => "Joe Doe",
+                    "img_url" => "https://i.pravatar.cc/300"
+                ],
+                "nbr_reponses" => 4
+            ],
+            [
+                "titre" => "Title",
+                "contenu" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum asperiores quod quis nisi, ab beatae ipsum sint obcaecati ipsam, modi officia excepturi voluptas, corporis at tempore fugit exercitationem repudiandae ipsa?",
+                "vote" => -12,
+                "auteur" => [
+                    "nom" => "Joe Doe",
+                    "img_url" => "https://i.pravatar.cc/300"
+                ],
+                "nbr_reponses" => 4
+            ]
+        ];
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
